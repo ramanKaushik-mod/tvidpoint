43bf7a89246bcc9f506031a15491ab9a16ce71fe
ramanKaushik-mod <81973853+ramanKaushik-mod@users.noreply.github.com>
2021-09-24 19:26:08 +0530

    Finalized

--- tvidpoint/lib/constants.dart
+++ tvidpoint/lib/constants.dart
@@ -1,8 +1,6 @@
 class Constants {
-  static const String API_KEY = "AIzaSyDgzxYJNlQmLqXIoVgM7fLsFoyjED6U3Es";
+  static const String API_KEY = "AIzqXFoyjED6U3aSyDgzxYJNlQmLEsIoVgM7fLs";
   static String userID = 'USER_ID';
   static String onBoardVisit = "ON_BOARD_VISIT";
   static String signedIn = "SIGNED_IN";
 }
-
-//constants
